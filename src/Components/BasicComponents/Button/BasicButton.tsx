33282aab857b4e1f772dfe90a6cd7cf0eabf6fa0
Adithya Krishna <adikrish@redhat.com>
2022-10-29 02:34:29 +0530

    Created Basic Buttons
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- src/Components/BasicComponents/Button/BasicButton.tsx
+++ src/Components/BasicComponents/Button/BasicButton.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Button from '@mui/material/Button';
 
 interface ButtonProps {
-    variant?: "outlined" | "text" | "contained"
+    variant?: "outlined" | "text" | "contained";
     isDisabled?: boolean;
     buttonText: string;
-    onClick: React.MouseEventHandler<HTMLButtonElement>;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
     color?: "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning" | undefined;
-    startIcon: React.ReactNode;
-    endIcon: React.ReactNode;
+    startIcon?: React.ReactNode;
+    endIcon?: React.ReactNode;
+    className?: string;
 }
 
 export const BasicButton = (props: ButtonProps) => {
   return (
-    <Button variant={props.variant} startIcon={props.startIcon} endIcon={props.endIcon} color={props.color} onClick={props.onClick} disabled={props.isDisabled}>{props.buttonText}</Button>
+    <Button className={props.className} variant={props.variant} startIcon={props.startIcon} endIcon={props.endIcon} color={props.color} onClick={props.onClick} disabled={props.isDisabled}>{props.buttonText}</Button>
   );
 }
