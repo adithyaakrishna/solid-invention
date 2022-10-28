597f050267d28871479c9e9af0c692b9d4b94c2f
Adithya Krishna <adikrish@redhat.com>
2022-10-29 02:35:06 +0530

    Added Message Node
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- src/Components/Nodes/MessageNode/MessageNode.tsx
+++ src/Components/Nodes/MessageNode/MessageNode.tsx
@@ -2,24 +2,35 @@
 import { useCallback } from 'react';
 import { Handle, Position } from 'reactflow';
 import './messageNode.min.css';
-import MessageNodeCard from './MessageNodeCard';
+import Whatsapp from '../../../Assets/Icons/WA.png';
+import Chat from '../../../Assets/Icons/Chat.png';
+import { Typography } from '@mui/material';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const MessageNode= () =>{
-  const onChange = useCallback((evt: any) => {
-    console.log(evt.target.value);
-  }, []);
+export const MessageNode = () =>{
 
   return (
-    <>
-        <div className='message-node'>
+    <div className='message-node'>
+      <div className='message-node-header'>
             <Handle type="target" position={Position.Right} />
-            <div>
-                <MessageNodeCard />
+            <div className='node-header'>
+              <img src={Chat} alt="Whatsapp"  className="chat-image" />
+              <span>
+                Send Message
+              </span>
+              <span className="white-background">
+                <img src={Whatsapp} alt="Whatsapp"  className="whatsapp-image" />
+              </span>
             </div>
             <Handle type="source" position={Position.Left} id="a" />
+            
         </div>
-    </>
+        <div className='message-text'>
+          <Typography variant="subtitle2" gutterBottom>
+            Dummy Message
+          </Typography>
+        </div>
+    </div>
   );
 }
 
